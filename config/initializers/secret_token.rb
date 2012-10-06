539c78df033171d0b3c6185f63d86bcbfea14cbf
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-10-06 10:41:26 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PingPong::Application.config.secret_token = '8be9b5a9e491f698cd7ffe0693976a9eaf5177983b064fdfabf2e0a6d5bec08d58b6c467199ff0b9b64ee848e7b0caef5bf34794ed9e21711259e824c29095cc'
