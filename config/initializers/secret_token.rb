b7404875ba6fc70f9b6841d2927039574c546f1c
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-10-07 21:08:47 +0900

    security

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PingPong::Application.config.secret_token = '8be9b5a9e491f698cd7ffe0693976a9eaf5177983b064fdfabf2e0a6d5bec08d58b6c467199ff0b9b64ee848e7b0caef5bf34794ed9e21711259e824c29095cc'
+
+require '/var/app/setting/ping_pong/secret_token.rb'
+PingPong::Application.config.secret_token = ping_pong_secret_token
