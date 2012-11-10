f54d5304a8103e0a4ca6d4eba6c077ba6a5ead70
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-11-10 14:58:36 +0900

    条件付きsingles -controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -11,9 +11,50 @@ class MatchesController < ApplicationController
   def generate
     attendance = cookies['attendance'].split(',')
     attendance.shift
-    male = generate_singles(Member.where(id: attendance, member_gender: true))
-    female = generate_singles(Member.where(id: attendance, member_gender: false))
-    render json: { '男子' => male, '女子' => female }
+
+    match_type = cookies['match_type']
+    match_gender = cookies['match_gender']
+    match_rank = cookies['match_rank']
+
+    if match_type == 'singles'
+      if match_gender == 'separate'
+        if match_rank == 'separate'
+          male_1 = generate_singles(Member.where(id: attendance, member_gender: true, rank_id: 1))
+          male_2 = generate_singles(Member.where(id: attendance, member_gender: true, rank_id: 2))
+          male_3 = generate_singles(Member.where(id: attendance, member_gender: true, rank_id: 3))
+          female_1 = generate_singles(Member.where(id: attendance, member_gender: false, rank_id: 1))
+          female_2 = generate_singles(Member.where(id: attendance, member_gender: false, rank_id: 2))
+          female_3 = generate_singles(Member.where(id: attendance, member_gender: false, rank_id: 3))
+          render json: {
+            '男子初級' => male_1,
+            '男子中級' => male_2,
+            '男子上級' => male_3,
+            '女子初級' => female_1,
+            '女子中級' => female_2,
+            '女子上級' => female_3
+          }
+        elsif match_rank == 'cross'
+          # TODO
+          render json: { status: :pending }
+        else
+          male = generate_singles(Member.where(id: attendance, member_gender: true))
+          female = generate_singles(Member.where(id: attendance, member_gender: false))
+          render json: { '男子' => male, '女子' => female }
+        end
+      else
+        if match_rank == 'separate'
+          rank_1 = generate_singles(Member.where(id: attendance, rank_id: 1))
+          rank_2 = generate_singles(Member.where(id: attendance, rank_id: 2))
+          rank_3 = generate_singles(Member.where(id: attendance, rank_id: 3))
+          render json: { '初級' => rank_1, '中級' => rank_2, '上級' => rank_3 }
+        elsif match_rank == 'cross'
+          # TODO
+          render json: { status: :pending }
+        else
+          render json: { 'ランダム対戦' => generate_singles(Member.where(id: attendance)) }
+        end
+      end
+    end
   end
 
 private
